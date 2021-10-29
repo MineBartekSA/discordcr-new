85b811f0f81dd99bf3533f7b10b3eeb02bb35541
Bartłomiej Skoczeń <bartek_skoczen@protonmail.com>
2021-10-29 17:28:18 +0200

    Change Slash Commands to Application Commands

--- src/discordcr/mappings/message_components.cr
+++ src/discordcr/mappings/message_components.cr
@@ -4,16 +4,20 @@ module Discord
 
     @[JSON::Field(converter: Enum::ValueConverter(Discord::ComponentType))]
     property type : ComponentType
+    property custom_id : String?
+    property disabled : Bool?
     @[JSON::Field(converter: Enum::ValueConverter(Discord::Style))]
     property style : Style?
     property label : String?
     property emoji : Emoji?
-    property custom_id : String?
     property url : String?
-    property disabled : Bool?
+    property options : Array(SelectOption)?
+    property placeholder : String?
+    property min_values : Int32?
+    property max_values : Int32?
     property components : Array(Component)?
 
-    def initialize(@type, @style = nil, @label = nil, @emoji = nil, @custom_id = nil, @url = nil, @disabled = nil, @components = nil)
+    def initialize(@type, @custom_id = nil, @disabled = nil, @style = nil, @label = nil, @emoji = nil, @url = nil, @options = nil, @placeholder = nil, @min_values = nil, @max_values = nil, @components = nil)
     end
 
     def self.action_row(components = nil)
@@ -21,13 +25,18 @@ module Discord
     end
 
     def self.button(style = nil, label = nil, emoji = nil, custom_id = nil, url = nil, disabled = nil)
-      self.new(ComponentType::Button, style, label, emoji, custom_id, url, disabled)
+      self.new(ComponentType::Button, custom_id, disabled, style, label, emoji, url)
+    end
+
+    def self.select_menu(custom_id = nil, options = nil, placeholder = nil, min_values = nil, max_values = nil)
+      self.new(ComponentType::SelectMenu, custom_id, options: options, placeholder: placeholder, min_values: min_values, max_values: max_values)
     end
   end
 
   enum ComponentType
-    ActionRow = 1
-    Button    = 2
+    ActionRow  = 1
+    Button     = 2
+    SelectMenu = 3
   end
 
   enum Style
@@ -36,5 +45,18 @@ module Discord
     Success   = 3
     Danger    = 4
     Link      = 5
+  end
+
+  struct SelectOption
+    include JSON::Serializable
+
+    property label : String
+    property value : String
+    property description : String?
+    property emoji : Emoji?
+    property default : Bool?
+
+    def initalize(@label, @value, @description = nil, @emoji = nil, @default = nil)
+    end
   end
 end
